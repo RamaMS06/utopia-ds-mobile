2986f1436a45bd1954e56e747a195ef3ea1a1c70
p-rama.msugiyanto <p-rama.msugiyanto@bukitmakmur.com>
2025-05-13 22:52:26 +0700

    Refactor public file for responsive helper

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 ## [Beta]
 
+## 0.0.1-beta.3
+- Refactor public file for responsive helper
+
 ## 0.0.1-beta.2
 - Rename UILabel to UIText
 
--- lib/helper/responsive/responsive.dart
+++ lib/helper/responsive/responsive.dart
@@ -1,7 +1,3 @@
 // All directories or files in [helper/responsive] is from flutter_screenutil
-export '_flutter_widgets.dart';
-export 'screen_responsive.dart';
 export 'screen_responsive_init.dart';
-export 'screen_responsive_mixin.dart';
 export 'size_extension.dart';
-export 'ut_padding.dart';
